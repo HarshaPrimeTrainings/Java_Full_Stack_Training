cebe9ce71044ba3fd240a21fa38fb3ebc1bc25b3
HarshaVardhan <athaluri.harsha@gmail.com>
2023-02-02 21:27:48 +0530

    VariableDemo

--- DemoApp.java
+++ DemoApp.java
@@ -1,4 +1,4 @@
-class MyApp{
+class DemoApp{
 	public static void main(String[] args){
 		System.out.println("Hello There!!");
 	}
---
+++ VariableDemo.java
@@ -0,0 +1,19 @@
+class  VariableDemo{
+	public static void main(String[] args){
+		
+		int a = 4; 
+		double b = 5.5;
+		char c = 'q';
+		boolean d = true; 
+		System.out.println("Intiger: " + a);
+		System.out.println("Double : " + b);
+		System.out.println("Char : " + c);
+		System.out.println("Boolean : " + d);
+		
+		
+		
+		
+	}
+		
+		
+}
